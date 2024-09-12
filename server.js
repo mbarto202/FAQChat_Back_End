cc6d1c787fec0c1b0779ea8e723f3275d232e826
mbarto202 <michael.d.barto@gmail.com>
2024-09-11 22:56:56 -0400

    server connected to db

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-const express = require('express');
-const {Pool} = require('pg');
+import express from 'express';
+import dotenv from 'dotenv';
+import pkg from 'pg';
+const { Pool } = pkg;
+
+dotenv.config(); // Load environment variables
 
 const app = express();
 const port = 3000;
 
 // connection setup/postgres info
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'faqchat_db',
-    password: 'mysecretpassword',
-    port: 5432,
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
 });
 
 // Testing connection
 pool.connect((err, client, release) => {
     if (err) {
-        return console.error('Error acquiring client', err.stack);
+        console.error('Error acquiring client', err.stack);
+    } else {
+        client.query('SELECT NOW()',(err, result) => {
+            release();
+            if(err){
+                console.error('error executing query', err.stack);
+            } else {
+                console.log('Connected to the database:', result.rows);
+            }
+        });
     }
-    client.query('SELECT NOW()',(err, result) => {
-        release();
-        if(err){
-            return console.error('error executing query', err.stack);
-        }
-        console.log('Connected to the database:', result.rows);
-    });
-})
+});
 
 // Route to home
 app.get('/', (req, res) => {
@@ -33,13 +39,13 @@ app.get('/', (req, res) => {
 });
 
 // Route to db connection
-app.get('/db', (req, res) => {
+app.get('/test-db-connection', (req, res) => {
     pool.query('SELECT NOW()', (err, result) => {
         if(err) {
             console.error('Error executing query', err.stack);
             res.status(500).send('Error connecting to the database');
         } else {
-            res.send(`Databse connected! Current time: ${result.rows[0].now}`);
+            res.status(200).send(`Database connected! Current time: ${result.rows[0].now}`);
         }
     });
 });
@@ -48,3 +54,5 @@ app.get('/db', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+export default app;
